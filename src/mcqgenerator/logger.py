ddf7e70757129e7336fff12a2c9fde2bbdd80939
Thoufiq-Developer <thoufiq2005ahamed@gmail.com>
2025-07-19 19:29:31 +0530

    updated

--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -5,3 +5,8 @@ LOG_file=f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
 
 log_path=os.path.join(os.getcwd(),"logs")
 os.makedirs(log_path,exist_ok=True) 
+LOG_FILE_PATH = os.path.join(log_path, LOG_file)
+
+logging.basicConfig(level=logging.INFO,
+       filename=LOG_FILE_PATH,
+       format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s" )   
